002ab9ff194c6764e5b5310c2ac9bd6abb62d2ab
denis909 <denis909@mail.ru>
2020-04-25 13:01:39 +0300

    n/a

---
+++ DeleteRelatedBehavior.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace denis909\yii;
+
+use yii\db\ActiveRecord;
+
+class DeleteRelatedBehavior extends \yii\base\Behavior
+{
+
+    public $relations = [];
+
+    public function events()
+    {
+        return [
+            ActiveRecord::EVENT_BEFORE_DELETE => 'beforeDelete'
+        ];
+    }
+
+    public function beforeDelete($event)
+    {
+        foreach($this->relations as $name)
+        {
+            $value = $this->owner->{$name};
+
+            if (is_array($value))
+            {
+                foreach($value as $related)
+                {
+                    if (!$value->delete())
+                    {
+                        $event->isValid = false;
+                    }
+                }
+            }
+            else
+            {
+                if ($value)
+                {
+                    if (!$value->delete())
+                    {
+                        $event->isValid = false;
+                    }
+                }
+            }
+        }
+    }
+
+}
